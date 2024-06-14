01e3bb04b052085f03d28e6901e7ca341500e690
alexwalk01 <alexitokira.prod@gmail.com>
2024-06-14 10:24:42 -0600

    subiendo cambios desde el repositorio

--- src/resources-python/view/main.py
+++ src/resources-python/view/main.py
@@ -10,7 +10,7 @@ project_dir = os.path.dirname(script_dir)
 sys.path.append(project_dir)
 # -----------------------------------------------------------------------------------------------
 
-from vista import main  # Importa la función main de vista.py
+from vista import main
 
 if __name__ == "__main__":
     main()  # Ejecuta la función main de vista.py
